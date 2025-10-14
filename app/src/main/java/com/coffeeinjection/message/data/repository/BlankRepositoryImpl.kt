3ae4038d8b5a61ad9da70a174ec1f0a42ae4f968
추민수 <devsoftychoo@gmail.com>
2025-10-14 21:40:28 +0900

    add import

--- app/src/main/java/com/coffeeinjection/message/data/repository/BlankRepositoryImpl.kt
+++ app/src/main/java/com/coffeeinjection/message/data/repository/BlankRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.coffeeinjection.message.data.repository
 
+import com.coffeeinjection.message.domain.repository.BlankRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 class BlankRepositoryImpl @Inject constructor(
     // todo api, dao 연결 예정
