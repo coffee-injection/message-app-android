dcd513808e406764ebb7c265bbb071b83d5f151e
김영현 <gim-yeonghyeon@gim-yeonghyeon-ui-MacBookPro-2.local>
2025-12-14 20:56:17 +0900

    fix : 버튼 색상 변경

--- app/src/main/java/com/coffeeinjection/message/util/GradientTextButton.kt
+++ app/src/main/java/com/coffeeinjection/message/util/GradientTextButton.kt
@@ -39,41 +39,12 @@ class GradientTextButton @JvmOverloads constructor(
 
         // XML에 android:background가 없으면 기본 배경 적용
         if (!hasAndroidAttribute(attrs, android.R.attr.background)) {
-            setBackgroundResource(R.drawable.btn_white_round)
+            setBackgroundResource(R.drawable.btn_gradient_blue)
         }
 
         // 텍스트 정렬
         if (!hasAndroidAttribute(attrs, android.R.attr.gravity)) {
             gravity = Gravity.START
-        }
-
-//        // 1) 아이콘 커스텀 속성 우선
-//        context.obtainStyledAttributes(attrs, R.styleable.GradientTextButton, defStyleAttr, 0).apply {
-//            iconStart = getDrawable(R.styleable.GradientTextButton_ciIcon) //
-//            iconTint = getColorStateList(R.styleable.GradientTextButton_ciIconTint) // 아이콘 색
-//            iconSizePx = getDimensionPixelSize(R.styleable.GradientTextButton_ciIconSize, 0) // 아이콘 크기
-//            iconPaddingPx = getDimensionPixelSize( // 텍스트와 아이콘 간격
-//                R.styleable.GradientTextButton_ciIconPadding, iconPaddingPx
-//            )
-//            recycle()
-//        }
-
-        // 2) 커스텀 아이콘이 없으면 프레임워크 속성으로 폴백
-        if (iconStart == null && attrs != null) {
-            // drawableStart(0) -> drawableLeft(1) -> src(2) 순으로 시도
-            val a = context.obtainStyledAttributes(
-                attrs, intArrayOf(
-                    android.R.attr.drawableStart,
-                    android.R.attr.drawableLeft,
-                    android.R.attr.src // Button에선 보통 값이 없음
-                )
-            )
-            val d = try {
-                a.getDrawable(0) ?: a.getDrawable(1) ?: a.getDrawable(2)
-            } finally {
-                a.recycle()
-            }
-            if (d != null) iconStart = d
         }
 
         compoundDrawablePadding = iconPaddingPx
